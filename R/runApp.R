c33ae452d84f79623bd630e56267d3edd515c9df
Leonardo Chalhoub <leochalhoub@hotmail.com>
2023-03-13 09:25:26 -0300

    teste

--- R/runApp.R
+++ R/runApp.R
@@ -1,6 +1,5 @@
 #' @export
 shinyApp_Passo4 <- function(pasta) {
-  pasta = pasta
   appDir <- system.file("myapp", package = "getPBFData")
   if (appDir == "") {
     stop("Could not find myapp. Try re-installing `mypackage`.", call. = FALSE)
