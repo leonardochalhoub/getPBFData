a9bb017c95e0362698946c999570e3a78cda8080
Leonardo Chalhoub <leonardo.chalhoub-ext@ab-inbev.com>
2026-04-06 02:47:47 -0300

    Web: Baixar Imagens PNG + cleanup assets + Dockerfile

--- legacy_R_code/R/Passo4_runAppShiny.R
+++
@@ -1,9 +0,0 @@
-#' @export
-shinyApp_Passo4 <- function(pasta) {
-  appDir <- system.file("myapp", package = "getPBFData")
-  if (appDir == "") {
-    stop("Could not find myapp. Try re-installing `mypackage`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
